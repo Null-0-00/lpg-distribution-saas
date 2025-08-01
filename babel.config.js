0a52fee2b1d96935f12eb4d9a743b05626a8a37f
j <devwork404@gmail.com>
2025-08-01 09:07:58 +0600

    mvp1.013

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-private-methods',
+    '@babel/plugin-syntax-import-attributes',
     'babel-plugin-transform-dynamic-import',
   ],
 };
